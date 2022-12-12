1911cc9f3b74a407d3ec4b2efbab73e890b3f7a4
German <german.glumov@gmail.com>
2022-12-12 22:10:51 +0300

    первый

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,20 +1,26 @@
-﻿Console.Write("Введите число A от 0 до 10: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B от 0 до 10: ");
-int b = Convert.ToInt32(Console.ReadLine());
-
-int result = a;
-
-if(a > 0 && a < 10 && b > 0 && b < 10)
+﻿
+void FullArray(int a, int b, int result)
 {
-    for(int i = 1; i < b; i++)
+    if(a > 0 && a < 10 && b > 0 && b < 10)
     {
-        result = result * a;
+        result = a;
+        for(int i = 1; i < b; i++)
+        {   
+            result = result * a;
+        }
     }
-}
-else
-{
-    Console.WriteLine("Введено неверное число");
+    else
+    {
+        Console.WriteLine("Введено неверное число");
+    }
+    Console.WriteLine(result);
 }
 
-Console.Write(result);
+Console.Write("Введите число A от 0 до 10: ");
+int v = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B от 0 до 10: ");
+int g = Convert.ToInt32(Console.ReadLine());
+
+int l = v;
+
+FullArray(v, g, l);
